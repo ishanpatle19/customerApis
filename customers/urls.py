e7815df6788cb89f1a18df4471089650d7cf88ca
ishanpatle19 <ishan@numeroeins.com>
2021-02-01 17:48:06 +0530

    working

---
+++ customers/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from customers import views
+
+from django.urls import path, include
+
+urlpatterns = [
+    # url(r'^customers/$', views.customer_list),
+    url(r'^customers/(?P<pk>[0-9]+)$', views.customer_detail),
+    url(r'^customers', views.customer_list),
+    
+    path('login/', views.customer_detail),
+    path('register/', views.customer_list),
+    
+]
